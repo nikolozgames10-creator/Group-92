0369c5b6f7948715e3c79329de43b8e9a553928f
nikolozgames10-creator <nikolozgames10@gmail.com>
2026-02-03 22:40:17 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,109 @@
+from turtle import *
+
+print("9")
+
+print("9")  #string  #str
+
+print(9)    #integer int
+
+print(5 + int("5"))
+
+print("Cercva aris " + "9" + "wlis")
+
+# make a house
+speed(45)
+
+width(9)
+color("blue")
+#square
+forward(200)
+left(90) 
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)  #square end
+left(90)
+#door
+forward(75)
+color("green")
+left(90)
+
+forward(120)
+right(90)
+
+forward(60)
+right(90)   
+ 
+forward(120)   #door end
+
+
+penup()
+goto(200, 200)
+pendown()
+
+color("brown")
+
+right(150)
+forward(200)
+
+left(120)
+forward(200)
+
+penup()
+goto(200, 140)
+pendown()
+
+penup()
+goto(120, 135)
+pendown()
+
+
+left(120)
+forward(45)
+
+left(90)
+forward(45)
+
+left(90)
+forward(45)
+
+left(90)
+forward(45)
+
+penup()
+right(90)
+forward(45)
+pendown()
+
+forward(45)
+left(270)
+
+forward(45)
+right(90)
+
+forward(45)
+right(90)
+
+forward(45)
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+
+
+
+
+exitonclick()
