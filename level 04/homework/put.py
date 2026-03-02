a0597124b9d9aba0be97b665833ae890e934ca9a
nikolozgames10-creator <nikolozgames10@gmail.com>
2026-03-02 16:31:24 +0400

    second commit

---
+++ level 04/homework/put.py
@@ -0,0 +1,36 @@
+#რიცხვების შემოყვანა და საშუალლო არითმეტიკულის გამოთვლა
+num = 5
+num_1 = 10
+num_2 = 9
+num_3 = 8
+print((num + num_1 + num_2 + num_3)/4) 
+
+# მომხმარებლისგან ორი რიცხვის მიღება
+num1 = float(input("შემოიყვანეთ პირველი რიცხვი: "))
+num2 = float(input("შემოიყვანეთ მეორე რიცხვი: "))
+
+#მომხმარებლის რიცხვების ჯამის გამოთლა
+total = num1 + num2
+
+#ჯამი
+print(total)
+
+mother_name = input("შეიყვანეთ დედის ასაკი: ")
+father_name = input("შეიყვანეთ მამის ასაკი: ")
+my_name = input("შეიყვანეთ თქვენი სახელი: " )
+
+city = input("შეიყვანეთ თქვენი ქალაქი: " )
+district = input("შეიყვანეთ თქვენი უბანი: ")
+building = input("შემოიყვანეთ თქვენი კორპუსი: ")
+floor = input("შემოიყვანეთ თქვენი სართეული: ")
+
+print("ქალაქი", city)
+print("უბანი",district)
+print("კორპუსი", building)
+print("სართული", floor)
+
+age = input("შეიყვანეთ თქვენი ასაკი: ")
+print("you are", age , "years old")
+
+
+
