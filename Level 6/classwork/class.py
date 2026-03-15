6fc8cef2a074bbe5633bdcb95d8262648dfd01d0
nikolozgames10-creator <nikolozgames10@gmail.com>
2026-03-15 13:43:43 +0400

    second commit

---
+++ Level 6/classwork/class.py
@@ -0,0 +1,18 @@
+name = input("enter your name: ")
+print(name)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(num1 + num2)
+
+age = int(input("Enter your age: "))
+print(type(age))
+
+num = 10
+text = "Hello"
+high = 3.14
+
+print(type(num))
+print(type(text))
+print(type(high))
